6e6daf94c9e8e5b414bc4a600363b1222c9b4575
Said <said.aguilar619@gmail.com>
2020-04-11 11:32:51 -0700

    deleted stuff

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,9 +1,5 @@
 // Requiring path to so we can use relative routes to our HTML files
 var path = require("path");
-
-// Requiring our custom middleware for checking if a user is logged in
-var isAuthenticated = require("../config/middleware/isAuthenticated");
-
 module.exports = function(app) {
 
   app.get("/", function(req, res) {
@@ -23,11 +19,4 @@ module.exports = function(app) {
   app.get("/giving", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/givingBack.html"));
   });
-
-  // Here we've add our isAuthenticated middleware to this route.
-  // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/members", isAuthenticated, function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/members.html"));
-  });
-
 };
